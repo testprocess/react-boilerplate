c4f759f76a8bfdd8cc4771859060bd673ce48977
Dipokal HHJ <hhj@devent.kr>
2024-01-06 21:18:20 +0900

    init: clone repo

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+/** @jsxImportSource @emotion/react */
+
+import React, { useEffect, useState } from 'react';
+
+import { css } from '@emotion/react'
+import './App.css'
+
+
+function App() {
+    return (
+        <div css={css({
+            display: "flex",
+            backgroundColor: "#000000"
+        })}>
+            test
+        </div>
+    );
+};
+
+export default App;
